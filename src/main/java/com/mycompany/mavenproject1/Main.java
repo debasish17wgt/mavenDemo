59fa0eba39f18306746c56b19773c2cfefcae9d7
Debasish Nandi <debasish17.wgt@gamil.com>
2018-11-21 15:34:09 +0530

    some changes 2

--- src/main/java/com/mycompany/mavenproject1/Main.java
+++ src/main/java/com/mycompany/mavenproject1/Main.java
@@ -11,6 +11,6 @@ package com.mycompany.mavenproject1;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World 11");
+        System.out.println("Hello World 2");
     }
 }
